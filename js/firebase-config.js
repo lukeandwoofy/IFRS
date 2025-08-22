c7065d7a6850323b3ee47153331a5d48e36d6440
lukeandwoofy <ecocompany32@gmail.com>
2025-08-22 13:42:05 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,14 @@
 // Replace with your actual Firebase project config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  // other fields as provided by Firebase
+  apiKey: "AIzaSyD2-oIhFYCdCiSdNWupF_WFhlDK2NQD_P8",
+  authDomain: "ifrs-eaaa9.firebaseapp.com",
+  projectId: "ifrs-eaaa9",
+  storageBucket: "ifrs-eaaa9.firebasestorage.app",
+  messagingSenderId: "567898303529",
+  appId: "1:567898303529:web:b81a80462acedeac198038",
+  measurementId: "G-HVJZNCNC3Z"
 };
+
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
