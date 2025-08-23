6de0152fbb8c559f772256fb622afd2a335ecb39
lukeandwoofy <ecocompany32@gmail.com>
2025-08-23 12:40:41 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,10 @@
 // js/app.js
-
 import { auth } from './firebase-config.js';
 import { showAuth, showHome } from './ui.js';
 
 window.addEventListener('load', () => {
-  // Register service worker
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./service-worker.js');
   }
-
-  // Redirect based on auth state
-  auth.onAuthStateChanged(user => {
-    user ? showHome() : showAuth();
-  });
+  auth.onAuthStateChanged(user => user ? showHome() : showAuth());
 });
