96304c5dbdb2a3fc77df9c2ea9d37d4fa3dae0aa
lukeandwoofy <ecocompany32@gmail.com>
2025-08-23 12:40:22 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,4 +1,5 @@
-// Replace with your actual Firebase project config
+// js/firebase-config.js
+// Replace with your Firebase project credentials
 const firebaseConfig = {
   apiKey: "AIzaSyD2-oIhFYCdCiSdNWupF_WFhlDK2NQD_P8",
   authDomain: "ifrs-eaaa9.firebaseapp.com",
@@ -7,9 +8,7 @@ const firebaseConfig = {
   messagingSenderId: "567898303529",
   appId: "1:567898303529:web:b81a80462acedeac198038",
   measurementId: "G-HVJZNCNC3Z"
-};
+}
 
-
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth();
